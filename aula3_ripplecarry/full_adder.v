a79441eb7b553816c4650d24d75099ab7c175874
karkocode <rurubonan@gmail.com>
2026-09-01 11:29:27 -0300

    Modelagem Primitiva Exemplo de lógica usando a modelagem primitiva no lugar da modelagem dataflow

--- aula3_ripplecarry/full_adder.v
+++ aula3_ripplecarry/full_adder.v
@@ -7,7 +7,18 @@ module full_adder (
 );
 
 	//Modelagem dataflow
-	assign s = (a & b & ci) | (~a & b & ~ci) | (~a & ~b & ci) | (a & ~b & ~ci);
-	assign co = (a & b) | (ci & b) | (ci & a);
+	
+		//assign s = (a & b & ci) | (~a & b & ~ci) | (~a & ~b & ci) | (a & ~b & ~ci);
+		//assign co = (a & b) | (ci & b) | (ci & a);
+		
+	//Modelagem primitiva
+	
+		//carry out
+			
+			wire w1, w2, w3;
+			and #1 u1(w1, a, b);
+			and #1 u2(w2, ci, b);
+			and #1 u3(w3, ci, a);
+			or	 #1 u4(co, w1, w2, w3);
 
 endmodule 
